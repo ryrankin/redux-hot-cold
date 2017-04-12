de2617378d768c35773fa31e408a9d36eccb6530
Ryan Rankin <ryrankin@gmail.com>
2017-04-12 16:06:30 -0400

    4 actions

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,6 +1,23 @@
+// guess a number
+// add number guessed to list
+// generate new random number
+// submit guess
+
 export const NUMBER_GUESS = 'NUMBER_GUESS';
 export const numberGuess = value => ({
 	type: NUMBER_GUESS,
+	value: value
+});
+
+export const GUESSED_NUMBER = 'GUESSED_NUMBER';
+export const guessedNumber = value => ({
+	type: GUESSED_NUMBER,
+	value: value
+});
+
+export const NEW_NUMBER = 'NEW_NUMBER';
+export const newNumber = value => ({
+	type: NEW_NUMBER,
 	value: value
 });
 
