5d811710f68c4ae0bd536c306820c567b4c1c0d9
Ryan Rankin <ryrankin@gmail.com>
2017-04-12 13:10:20 -0400

    added actions

---
+++ js/actions/index.js
@@ -0,0 +1,11 @@
+export const NUMBER_GUESS = 'NUMBER_GUESS';
+export const numberGuess = value => ({
+	type: 'NUMBER_GUESS',
+	value: value
+});
+
+export const SUBMIT_GUESS = 'SUBMIT_GUESS';
+export const submitGuess = value => ({
+	type: SUBMIT_GUESS,
+	value: value
+});
