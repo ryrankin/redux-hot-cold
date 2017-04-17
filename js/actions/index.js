af5f5e1b6efd3b9d3e48500e8a015f8a35535ce1
Ryan Rankin <ryrankin@gmail.com>
2017-04-17 16:48:29 -0400

    reducers added

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,28 +1,16 @@
-// guess a number
+// usre guess
 // add number guessed to list
 // generate new random number
-// submit guess
 
-export const NUMBER_GUESS = 'NUMBER_GUESS';
-export const numberGuess = value => ({
-	type: NUMBER_GUESS,
-	value: value
+
+export const USER_GUESS = 'USER_GUESS';
+export const userGuess = guess => ({
+	type: USER_GUESS,
+	guess
 });
 
-export const GUESSED_NUMBER = 'GUESSED_NUMBER';
-export const guessedNumber = value => ({
-	type: GUESSED_NUMBER,
-	value: value
+export const NEW_GAME = 'NEW_GAME';
+export const newGame = () => ({
+	type: NEW_GAME
 });
 
-export const NEW_NUMBER = 'NEW_NUMBER';
-export const newNumber = value => ({
-	type: NEW_NUMBER,
-	value: value
-});
-
-export const SUBMIT_GUESS = 'SUBMIT_GUESS';
-export const submitGuess = value => ({
-	type: SUBMIT_GUESS,
-	value: value
-});
---
+++ js/reducers/index.js
@@ -0,0 +1,50 @@
+import * as actions from '../actions/index';
+
+const initialState = {
+	feedback: 'Make a guess',
+	guess: '',
+	guessList: [],
+	rightAnswer: Math.floor(Math.random() * 100),
+	won: false
+};
+
+export const gameReducer = (state=initialState, action)
+	if(action.type === actions.USER_GUESS){
+		const guess = parseInt(action.guess, 10);
+		console.log('guess', guess);
+		console.log(state.rightAnswer);
+		const difference = Math.abs(guess - state.rightAnswer);
+		let feedback;
+		let won = state.won;
+
+		if(difference > 50){
+			feedback = 'VERY COLD'
+		}
+		else if(difference > 30){
+			feedback = 'WARM'
+		}
+		else if(difference >= 1){
+			feedback = 'HOT'
+		}
+		else {
+			feedback = 'Right Answer';
+			won = 'true';
+		}
+
+		return {...state, 
+				feedback: feedback,
+				guess: guess,
+				guessList: state.guessList.concat(guess),
+				won: won
+			}
+
+	}
+	else if(action.type === actions.NEW_GAME){
+		return {...initialState,
+				rightAnswer: Math.floor((Math.random() * 100))
+			}
+	}
+
+	return state;
+
+}
