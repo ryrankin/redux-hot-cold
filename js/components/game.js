6f0f919eecb1c094f598cbed23d35da4d02dd981
Ryan Rankin <ryrankin@gmail.com>
2017-04-17 18:26:20 -0400

    added game component

---
+++ js/components/game.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {newGame} from '..actions/index';
+
+export class Game extends React.Component{
+	render(){
+		let won;
+		if(this.props.won){
+			won = (<button onClick={(event)=>{this.props.dispatch(newGame())}} value="New Game">New Game</button>)
+		}
+		console.log(this.props.rightAnswer);
+		return(
+				<div>
+					<h1>{this.props.feedback}</h1>
+					{won}
+					<FormContainer />
+					<GuessList guessList={this.props.guessList} />
+				</div>
+		)
+	}
+}
+
+const mapStateToProps = (state, props) => ({
+	feedback: state.feedback,
+	guessList: state.guessList,
+	won: state.won,
+	rightAnswer: state.rightAnswer
+})
+
+export default connect(mapStateToProps)(Game);
