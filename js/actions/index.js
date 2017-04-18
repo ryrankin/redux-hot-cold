76de734781b2edfbde6b1494ac4203b48500b674
Ryan Rankin <ryrankin@gmail.com>
2017-04-18 19:39:25 -0400

    added POST endpoint

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,4 +1,4 @@
-// usre guess
+// user guess
 // add number guessed to list
 // generate new random number
 
@@ -13,4 +13,50 @@ export const NEW_GAME = 'NEW_GAME';
 export const newGame = () => ({
 	type: NEW_GAME
 });
+
+export const POST_FEWEST_GUESSES_SUCCESS = 'POST_FEWEST_GUESSES_SUCCESS';
+export const postFewestGuessesSuccess = (message, fewestGuesses) => ({
+	type: POST_FEWEST_GUESSES_SUCCESS,
+	message,
+	fewestGuesses
+});
+
+export const POST_FEWEST_GUESSES_ERROR = 'POST_FEWEST_GUESSES_ERROR';
+export const postFewestGuessesError = error => ({
+	type: POST_FEWEST_GUESSES_ERROR,
+	error
+});
+
+export const postFewestGuesses = fewestGuesses => dispatch => {
+	fetch('/fewest-guesses' , {
+		method: 'POST',
+		headers: {
+				'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({fewestGuesses: fewestGuesses})
+	}).then(response => {
+		if(!response.ok){
+			let error = new Error(response.statusText);
+			error.response = response;
+			throw error;
+		}
+	})
+		.then(response => response.json())
+		.then(data => {
+			dispatch(postFewestGuessesSuccess(data.message, data.fewestGuesses));
+			console.log(data);
+		})
+		.catch(error => {
+			dispatch(postFewestGuessesError(error));
+		})
+}
+
+
+
+
+
+
+
+
+
 
